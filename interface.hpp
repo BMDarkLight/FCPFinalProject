662ed240bd1a8bbda2d9d64719479011a852b7ce
Behdad <behdadmehrnia@gmail.com>
2025-02-01 02:29:20 +0330

    Sad Simplification

--- interface.hpp
+++
@@ -1,9 +0,0 @@
-#ifdef INTERFACE_HPP
-#else
-#define INTERFACE_HPP
-
-using namespace std;
-
-
-
-#endif
--- main.cpp
+++ main.cpp
@@ -1,22 +1,16 @@
 #include <iostream>
-#include <fstream>
 #include <iomanip>
+#include <fstream>
 #include <string>
 #include <vector>
 
-#include "interface.hpp"
 #include "data.hpp"
-#include "save.hpp"
-#include "read.hpp"
 
 using namespace std;
 
 int main () {
-    Data data = load();
+    
+    
 
-    data.listStudents();
-
-    string name; int id; string field;
-    cin >> name >> id >> field;
-    data.registerStudent(name, id, field);
+    system("pause");
 }
