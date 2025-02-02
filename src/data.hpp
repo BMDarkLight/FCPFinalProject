4749e1d4fb8c6566b35c6dbd53f2314c625ef4ef
rohersmith <rohersmith101010@gmail.com>
2025-02-02 14:31:17 +0330

    bad idea

--- src/data.hpp
+++ src/data.hpp
@@ -15,19 +15,5 @@ struct Student {
     int id;
     string field;
     vector <Subject> subjects;
-
-float score_gen() {
-        if (subjects.size() == 0) {
-            return 0;
-        }
-        float score = 0;
-        for (auto subject : subjects)
-            score += subject.score * subject.multiplier;
-        score /= subjects.size();
-
-        return score;
-    } float score = score_gen();
 };
-
-
 #endif
