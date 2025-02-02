9d09df8bb20f0bbd74af4fc9a6d98fa024b3c692
rohersmith <rohersmith101010@gmail.com>
2025-02-02 13:33:05 +0330

    function to calculate the avg score

--- src/data.hpp
+++ src/data.hpp
@@ -17,5 +17,18 @@ struct Student {
     vector <Subject> subjects;
 };
 
+float score_gen() {
+        if (subjects.size() == 0) {
+            return 0;
+        }
+        float score = 0;
+        for (auto subject : subjects)
+            score += subject.score * subject.multiplier;
+        score /= subjects.size();
+
+        return score;
+    } float score = score_gen();
+};
+
 
 #endif
